de5c7e00f75b301963265b8c3205a4dc322e521b
winds.from.oz@gmail.com <winds.from.oz@gmail.com>
2021-07-21 21:36:42 +0900

    20210721

---
+++ output_slack.go
@@ -0,0 +1,48 @@
+package main
+
+/*
+Slackに出力する
+*/
+
+import (
+  "fmt"
+	"log"
+	"time"
+
+	"github.com/gorilla/feeds"
+  "github.com/mmcdole/gofeed"
+)
+
+/*
+標準出力に出力
+*/
+func OutputSlack(feed []*gofeed.Feed) {
+
+  for _, f := range feed {
+	  c1 := f
+	  now := time.Now()
+	  output_feed := &feeds.Feed{
+	  	Title:       c1.Title,
+	  	Link:        &feeds.Link{Href: c1.Link},
+	  	Description: c1.Description,
+	  	Created:     now,
+	  }
+
+	  for _, v := range c1.Items {
+	  	item := &feeds.Item{
+	  		Title:       v.Title,
+	  		Link:        &feeds.Link{Href: v.Link},
+	  		Description: v.Description,
+	  		Created:     now,
+	  	}
+	  	output_feed.Add(item)
+	  }
+
+	  rss, err := output_feed.ToRss()
+	  if err != nil {
+	  	log.Fatal(err)
+	  }
+    fmt.Print(rss)
+  }
+
+}
