bb0b27e143c6740bbb8590cf516d8c676af5fed8
kaoru-inoue <winds.from.oz@gmail.com>
2022-05-21 22:57:37 +0900

    feed2cliにシンボリックリンクを貼る

--- feed2cli.go
+++ feed2cli.go
@@ -20,6 +20,12 @@ func main() {
 	// パイプのある無しで振る舞いを変える
 	if terminal.IsTerminal(0) {
 		fmt.Println("パイプ無し(FD値0)")
+		// -s だったらシンボリックリンクを作成する
+		if len(os.Args) > 1 && os.Args[1] == "-s" {
+			os.Symlink("feed2cli", "mergeRss")
+			os.Symlink("feed2cli", "diffRss")
+			os.Symlink("feed2cli", "slackRss")
+		}
 	} else {
 		//fmt.Println("パイプで渡された内容(FD値0以外):", string(b))
 		s := read()
