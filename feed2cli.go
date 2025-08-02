b3b91c7f27ca22687c4305877e309a96d2baa8e5
kaoru.inoue <winds.from.oz@gmail.com>
2025-08-02 23:58:27 +0900

    はてブの処理のリファクタリング

--- feed2cli.go
+++ feed2cli.go
@@ -13,7 +13,7 @@ import (
 func parseArgs() (isDebug, createSymlinks bool, operation, filterType string) {
 	flag.BoolVar(&isDebug, "d", false, "Debug output")
 	flag.BoolVar(&createSymlinks, "s", false, "Create symbolic links")
-	flag.StringVar(&operation, "o", "", "Operation: merge, diff, slack, or hatena")
+	flag.StringVar(&operation, "o", "", "Operation: merge, diff, slack, hatena, json")
 	flag.StringVar(&filterType, "f", "", "Filter to apply: hatena_bookmark")
 	flag.Parse()
 	return
@@ -32,27 +32,16 @@ func createSymlinksIfNeeded() {
 	_ = os.Symlink("feed2cli", "hatenaRss")
 }
 
-// applyFilter は、フィードのリストを受け取り、指定されたフィルタを適用して
-// FilteredItemのリストを返します。
 func applyFilter(filterType string, feeds []*gofeed.Feed) ([]*FilteredItem, error) {
 	var f Filter
 	switch filterType {
 	case "hatena_bookmark":
 		f = &HatenaBookmarkFilter{}
 	default:
-		// 未対応のフィルタの場合は何もしないが、型を変換する必要がある
-		items := []*gofeed.Item{}
-		for _, feed := range feeds {
-			items = append(items, feed.Items...)
-		}
-		filteredItems := make([]*FilteredItem, len(items))
-		for i, item := range items {
-			filteredItems[i] = &FilteredItem{Item: item}
-		}
-		return filteredItems, nil
+		// フィルタが指定されていない、または未対応の場合は、型変換のみ行う
+		return convertToFilteredItems(feeds), nil
 	}
 
-	// 全てのフィードからアイテムを一旦一つのスライスにまとめる
 	allItems := []*gofeed.Item{}
 	for _, feed := range feeds {
 		allItems = append(allItems, feed.Items...)
@@ -61,32 +50,32 @@ func applyFilter(filterType string, feeds []*gofeed.Feed) ([]*FilteredItem, erro
 	return f.Apply(allItems)
 }
 
-// dispatchOperation は、操作とデータを受け取り、適切な出力関数にディスパッチします。
-// データはフィルタリング済みか否かで型が異なるため、interface{}で受け取ります。
 func dispatchOperation(operation, cmd string, data interface{}) {
-	switch {
-	case cmd == "mergeRss" || operation == "merge":
-		// mergeとdiffはgofeed.Feedを期待するため、型変換が必要
+	op := operation
+	if op == "" {
+		op = strings.TrimSuffix(cmd, "Rss")
+	}
+
+	switch op {
+	case "merge":
 		feeds := convertToFeeds(data)
 		merged := Merge(feeds)
 		OutputStanderd(merged)
-	case cmd == "diffRss" || operation == "diff":
+	case "diff":
 		feeds := convertToFeeds(data)
 		diffed := Diff(feeds)
 		OutputStanderd(diffed)
-	case cmd == "slackRss" || operation == "slack":
-		// slackはgofeed.Feedを期待する
-		feeds := convertToFeeds(data)
-		OutputSlack(feeds)
-	case cmd == "hatenaRss" || operation == "hatena":
-		// hatenaはFilteredItemを期待する
+	case "slack":
+		OutputSlack(data)
+	case "hatena":
 		if items, ok := data.([]*FilteredItem); ok {
 			OutputHatenaToSlack(items)
 		} else {
-			fmt.Println("hatena操作にはフィルタリングされたデータが必要です。")
+			fmt.Fprintln(os.Stderr, "hatena操作にはフィルタリングされたデータが必要です。-f hatena_bookmark を使用してください。")
 		}
+	case "json":
+		OutputJSON(data)
 	default:
-		// デフォルトは標準出力
 		OutputStanderd(data)
 	}
 }
@@ -111,21 +100,22 @@ func main() {
 		s := read()
 		cmd := strings.TrimLeft(os.Args[0], "./")
 
+		var dataToDispatch interface{}
+		dataToDispatch = s // デフォルトは元のフィード
+
 		if filterType != "" {
 			filteredItems, err := applyFilter(filterType, s)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "フィルタの適用に失敗しました: %v\n", err)
 				os.Exit(1)
 			}
-			dispatchOperation(operation, cmd, filteredItems)
-		} else {
-			dispatchOperation(operation, cmd, s)
+			dataToDispatch = filteredItems
 		}
+
+		dispatchOperation(operation, cmd, dataToDispatch)
 	}
 }
 
-// convertToFeeds は、様々な型のデータを[]*gofeed.Feedに変換します。
-// これは、mergeやdiffなど、gofeed.Feedを直接操作する既存の関数との互換性を保つためです。
 func convertToFeeds(data interface{}) []*gofeed.Feed {
 	if feeds, ok := data.([]*gofeed.Feed); ok {
 		return feeds
@@ -136,10 +126,9 @@ func convertToFeeds(data interface{}) []*gofeed.Feed {
 		for i, item := range items {
 			feedItems[i] = item.Item
 		}
-		// 元のフィード情報は失われるが、一つのフィードにまとめる
 		return []*gofeed.Feed{{Items: feedItems}}
 	}
-	
+
 	if items, ok := data.([]*gofeed.Item); ok {
 		return []*gofeed.Feed{{Items: items}}
 	}
--- output_slack.go
+++ output_slack.go
@@ -1,128 +1,96 @@
 package main
 
-/*
-Slackに出力する
-
-todo
-* https://zenn.dev/kou_pg_0131/articles/slack-go-usage を参考にSlack通知のデザインを変えていく
-*/
-
 import (
 	"fmt"
 	"log"
-	"net/url"
 	"os"
-	"regexp"
+	"strings"
 	"time"
 
-	"github.com/gorilla/feeds"
 	"github.com/mmcdole/gofeed"
 	"github.com/slack-go/slack"
 )
 
-// toSlack は、フィードを受け取り、各アイテムをSlackに送信します。
-func toSlack(feed []*gofeed.Feed) {
+// OutputSlack は、フィードアイテムをSlackに通知します。
+// フィルタによって追加された情報があれば、それもメッセージに含めます。
+func OutputSlack(data interface{}) {
 	token := os.Getenv("XOXB")
 	if token == "" {
 		log.Fatal("環境変数XOXBにアクセストークンを設定してください。")
 	}
-	c := slack.New(token)
-
-	for _, f := range feed {
-		for _, v := range f.Items {
-			tagURL := processTags(v) // タグURLの処理
-
-			// Slackメッセージ用テキストを生成
-			markdownText := fmt.Sprintf("<%s|%s>\n%s", v.Link, v.Title, v.Description)
-
-			// Attachmentを生成
-			attachment := slack.Attachment{
-				ThumbURL: v.Extensions["hatena"]["imageurl"][0].Value,
-				Text:     formatAttachmentText(v, tagURL),
-			}
-
-			// Slackへメッセージを送信
-			attach := slack.MsgOptionAttachments(attachment)
-			sendSlackMessage(c, markdownText, attach)
-		}
-	}
-}
-
-// processTags は、フィードアイテム内のtagsを処理します。
-func processTags(v *gofeed.Item) string {
-	re := regexp.MustCompile(`.*?q=(.*)`)
-	li := v.Extensions["taxo"]["topics"][0].Children["Bag"][0].Children["li"]
-	tagURL := ""
-
-	for _, liV := range li {
-		res := re.FindAllStringSubmatch(liV.Attrs["resource"], -1)
-		if len(res) > 0 {
-			if str2, err := url.QueryUnescape(res[0][1]); err == nil {
-				tagURL += fmt.Sprintf("<%s|%s> ", liV.Attrs["resource"], str2)
-			} else {
-				log.Printf("タグURLのデコードに失敗しました: %v", err)
-			}
-		}
-	}
-	return tagURL
-}
-
-// formatAttachmentText は、Slackに送信するための添付テキストを整形します。
-func formatAttachmentText(v *gofeed.Item, tagURL string) string {
-	bookmarkCommentURL := v.Extensions["hatena"]["bookmarkCommentListPageUrl"][0].Value
-	bookmarkSiteURL := v.Extensions["hatena"]["bookmarkSiteEntriesListUrl"][0].Value
-
-	if bookmarkCommentURL == "" || bookmarkSiteURL == "" {
-		log.Println("コメントURLまたはサイトURLが空です。")
-	}
-
-	return fmt.Sprintf("<%s|コメント> <%s|関連>\n%s",
-		bookmarkCommentURL,
-		tagURL,
-		bookmarkSiteURL)
-}
-
-// sendSlackMessage は、Slackにメッセージを送信します。
-func sendSlackMessage(c *slack.Client, markdownText string, attach slack.MsgOption) {
-	channel := os.Getenv("SLACK_CHANNEL")
-	if channel == "" {
+	slackChannelID := os.Getenv("SLACK_CHANNEL")
+	if slackChannelID == "" {
 		log.Fatal("環境変数SLACK_CHANNELに通知先のチャンネル名またはユーザーIDを設定してください。")
 	}
-	channelID, timestamp, err := c.PostMessage(channel, slack.MsgOptionText(markdownText, false), attach, slack.MsgOptionAsUser(true))
-	if err != nil {
-		log.Printf("Slackへのメッセージ送信でエラーが発生しました: %v", err)
-	} else {
-		fmt.Printf("Message successfully sent to channel %s at %s\n", channelID, timestamp)
+
+	api := slack.New(token)
+
+	itemsToProcess := convertToFilteredItems(data)
+
+	for _, item := range itemsToProcess {
+		// Slackメッセージ用テキストを生成
+		// Descriptionは長くなる可能性があるので、Attachmentに含める
+		markdownText := fmt.Sprintf("<%s|%s>", item.Link, item.Title)
+
+		// Attachmentを生成
+		attachment := formatSlackAttachment(item)
+
+		// Slackへメッセージを送信
+		_, _, err := api.PostMessage(
+			slackChannelID,
+			slack.MsgOptionText(markdownText, false),
+			slack.MsgOptionAttachments(attachment),
+			slack.MsgOptionAsUser(true),
+		)
+		if err != nil {
+			log.Printf("Slackへのメッセージ送信でエラーが発生しました: %v", err)
+		}
+		// レートリミット対策
+		time.Sleep(1500 * time.Millisecond)
 	}
 }
 
-// OutputSlack は、フィードを受け取り、標準出力およびSlackに出力します。
-func OutputSlack(feed []*gofeed.Feed) {
-	for _, f := range feed {
-		now := time.Now()
-		outputFeed := &feeds.Feed{
-			Title:       f.Title,
-			Link:        &feeds.Link{Href: f.Link},
-			Description: f.Description,
-			Created:     now,
-		}
-		for _, v := range f.Items {
-			item := &feeds.Item{
-				Title:       v.Title,
-				Link:        &feeds.Link{Href: v.Link},
-				Description: v.Description,
-				Created:     now,
-			}
-			outputFeed.Add(item)
-		}
+// formatSlackAttachment は、FilteredItemからSlackのAttachmentを生成します。
+func formatSlackAttachment(item *FilteredItem) slack.Attachment {
+	var attachmentText strings.Builder
+	
+	// 元のDescriptionを追加
+	attachmentText.WriteString(item.Description)
+	attachmentText.WriteString("\n\n")
 
-		rss, err := outputFeed.ToRss()
-		if err != nil {
-			log.Fatalf("RSS生成に失敗しました: %v", err)
-		}
-		fmt.Print(rss)
-
-		// Slackへも出力
-		toSlack(feed) // ここでtoSlackを呼ぶ
+	// はてなブックマーク情報があれば追記
+	if item.HatenaBookmarkCount != "" && item.HatenaBookmarkCount != "0" {
+		attachmentText.WriteString(fmt.Sprintf("Hatena Bookmark: *%s*\n", item.HatenaBookmarkCount))
 	}
+	if len(item.HatenaBookmarkComments) > 0 {
+		// コメントが多すぎるとメッセージが長くなるため、件数のみ表示
+		attachmentText.WriteString(fmt.Sprintf("Hatena Comments: %d\n", len(item.HatenaBookmarkComments)))
+	}
+
+	return slack.Attachment{
+		Text: attachmentText.String(),
+	}
+}
+
+// convertToFilteredItems は、様々な型のデータを []*FilteredItem に変換します。
+// これにより、後続の処理が型を意識せずに済むようになります。
+func convertToFilteredItems(data interface{}) []*FilteredItem {
+	if filtered, ok := data.([]*FilteredItem); ok {
+		return filtered
+	}
+
+	var items []*gofeed.Item
+	if feeds, ok := data.([]*gofeed.Feed); ok {
+		for _, feed := range feeds {
+			items = append(items, feed.Items...)
+		}
+	} else if feedItems, ok := data.([]*gofeed.Item); ok {
+		items = feedItems
+	}
+
+	filteredItems := make([]*FilteredItem, len(items))
+	for i, item := range items {
+		filteredItems[i] = &FilteredItem{Item: item}
+	}
+	return filteredItems
 }
